b54666d4e1560409b9d4306049bf59aad496d9a4
Rojonso05 <alonsocancela@gmail.com>
2025-12-02 17:48:11 +0100

    Update menuScene.js

--- scripts/menuScene.js
+++ scripts/menuScene.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-
+import { GameSettings } from '../GameSettings.js';
 
 export class MenuScene extends Phaser.Scene {
     constructor() {
@@ -7,25 +7,92 @@ export class MenuScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(768, 100, 'HealFish', {
-            fontSize: '64px',
-            color: '#ffffff'
-        }).setOrigin(0.5);
+        
+        const cx = this.cameras.main.width / 2;
+        const cy = this.cameras.main.height / 2;
 
-        const localBtn = this.add.text(768, 320, 'Local-2 Player', {
-            fontSize: '24px',
-            color: '#00ff00',
-        }).setOrigin(0.5)
-        .setInteractive({useHandCursor: true})
-        .on('pointerover', () => localBtn.setColor('#00ff88'))
-        .on('pointerout', () => localBtn.setColor('#00ff00'))
-        .on('pointerdown', () => {
-            this.scene.start('GameScene');
+        this.clickSound = this.sound.add('clickSound', {
+            volume: GameSettings.sfxVolume
         });
 
-        const onlineBtn = this.add.text(768, 390, 'Online Multiplayer (Not available)', {
-            fontSize: '24px',
-            color: '#ff6666',
-        }).setOrigin(0.5);
+        // musica menu
+        GameSettings.playMusic(this, 'menuMs');
+
+        // fondo 
+        this.add.image(0, 0, 'menuBg')
+            .setOrigin(0, 0)
+            .setDisplaySize(
+                cx * 2, 
+                cy * 2
+            );
+        // logo
+        this.add.image(cx, cy, 'logoBg')
+            .setOrigin(0.5)
+            .setScale(0.7);
+
+        // boton de jugar
+        const playBtn = this.add.image(cx + 400, cy - 200, 'btnPlay')
+            .setScale(0.2)
+            .setOrigin(0.5)
+            .setInteractive({ useHandCursor: true });
+
+            playBtn.on('pointerover', () => { 
+                playBtn.setScale(0.24);
+
+                this.sound.play('hoverSound', {
+                    volume: GameSettings.sfxVolume,
+                    detune: Phaser.Math.Between(-20, 20)
+                }); 
+            });
+
+            playBtn.on('pointerout', () => playBtn.setScale(0.2));
+
+            playBtn.on('pointerdown', () => {
+                this.sound.play('clickSound', { volume: GameSettings.sfxVolume });
+                this.scene.start('InstructionScene');
+            });
+        
+        // boton de configuracion
+        const congifBtn = this.add.image(cx + 400, cy - 10, 'btnOptions')
+            .setScale(0.2) // tamaño base
+            .setOrigin(0.5)
+            .setInteractive({ useHandCursor: true });
+
+            congifBtn.on('pointerover', () => { 
+                congifBtn.setScale(0.24);
+
+                this.sound.play('hoverSound', {
+                    volume: GameSettings.sfxVolume,
+                    detune: Phaser.Math.Between(-20, 20)
+                }); 
+            });
+            congifBtn.on('pointerout', () => congifBtn.setScale(0.2));
+
+            congifBtn.on('pointerdown', () => {
+                this.sound.play('clickSound', { volume: GameSettings.sfxVolume });
+                this.scene.sleep(); 
+                this.scene.launch('ConfigurationScene', { previous: 'MenuScene' });
+            });
+
+        // boton de creditos
+        const creditsBtn = this.add.image(cx + 400, cy + 190, 'btnCredits')
+            .setScale(0.2) // tamaño base
+            .setOrigin(0.5)
+            .setInteractive({ useHandCursor: true });
+
+            creditsBtn.on('pointerover', () => { 
+                creditsBtn.setScale(0.24);
+
+                this.sound.play('hoverSound', {
+                    volume: GameSettings.sfxVolume,
+                    detune: Phaser.Math.Between(-20, 20)
+                }); 
+            });
+            creditsBtn.on('pointerout', () => creditsBtn.setScale(0.2));
+
+            creditsBtn.on('pointerdown', () => {
+                this.sound.play('clickSound', { volume: GameSettings.sfxVolume });
+                this.scene.start('CreditsScene');
+            });
     }
 }
