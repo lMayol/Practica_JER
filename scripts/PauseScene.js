15b236e0b5855cba73b0a32f5256e1409e6cc6f4
Rojonso05 <alonsocancela@gmail.com>
2025-12-02 17:45:07 +0100

    Update PauseScene.js

--- scripts/PauseScene.js
+++ scripts/PauseScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import { GameSettings } from '../GameSettings.js';
 
 export class PauseScene extends Phaser.Scene {
 
@@ -6,50 +7,152 @@ export class PauseScene extends Phaser.Scene {
         super('PauseScene');
     }
 
+    init(data) {
+        // escena que controla la pausa
+        this.parentSceneKey = data.parent || 'SplitScreenScene';
+    }
+
     create() {
-        this.add.rectangle(400, 300, 800, 600, 0x000000, 0.7);
 
-        this.add.text(400, 200, 'Game Paused', {
-            fontSize: '64px',
-            color: '#ffffff'
-        }).setOrigin(0.5);
+        // asegurar camara global superpuesta
+        this.cameras.main.setViewport(0, 0, this.scale.width, this.scale.height);
+        this.cameras.main.setScroll(0, 0);
 
-        // boton resume
-        const resumeBtn = this.add.text(400, 320, 'Resume', {
-            fontSize: '32px',
-            color: '#00ff00',
-        }).setOrigin(0.5)
-        .setInteractive({useHandCursor: true})
-        .on('pointerover', () => resumeBtn.setColor('#00ff88'))
-        .on('pointerout', () => resumeBtn.setColor('#00ff00'))
-        .on('pointerdown', () => {
-            this.scene.stop();               
-                this.scene.resume("GameScene");
+        const cx = this.cameras.main.width / 2;
+        const cy = this.cameras.main.height / 2;
+
+        this.clickSound = this.sound.add('clickSound', {
+            volume: GameSettings.sfxVolume
         });
 
-        // boton configuracion
-        const configurationBtn = this.add.text(400, 390, 'Options', {
-            fontSize: '32px',
-            color: '#00ff00',
-        }).setOrigin(0.5)
-        .setInteractive({useHandCursor: true})
-        .on('pointerover', () => configurationBtn.setColor('#00ff88'))
-        .on('pointerout', () => configurationBtn.setColor('#00ff00'))
-        .on('pointerdown', () => {
-            this.scene.sleep(); 
-            this.scene.launch("ConfigurationScene", { previous: "PauseScene" });
+        this.escKey = this.input.keyboard.addKey(
+            Phaser.Input.Keyboard.KeyCodes.ESC
+        );
+
+        // pausar sonidos
+        GameSettings.pauseMusic();
+
+        // fondo
+        this.add.image(0, 0, 'menuBg')
+            .setOrigin(0, 0)
+            .setDisplaySize(cx * 2, cy * 2);
+
+        // titulo pausa
+        this.add.image(cx + 50, cy - 350, 'pauseTit')
+            .setScale(0.8)
+            .setOrigin(0.5);
+
+        // chica
+        this.add.image(cx - 550, cy, 'chicIm')
+            .setScale(0.6)
+            .setOrigin(0.5);
+
+        // boton ajustes
+        const congifBtn = this.add.image(cx + 50, cy - 50, 'btnOptions')
+            .setScale(0.2)
+            .setOrigin(0.5)
+            .setInteractive({ useHandCursor: true });
+
+        congifBtn.on('pointerover', () => { 
+            congifBtn.setScale(0.24);        
+            this.sound.play('hoverSound', {
+                volume: GameSettings.sfxVolume,
+                detune: Phaser.Math.Between(-20, 20)
+            }); 
         });
 
-        const menuBtn = this.add.text(400, 460, 'Return to Main Menu', {
-            fontSize: '32px',
-            color: '#ffffff',
-        }).setOrigin(0.5)
-        .setInteractive({ useHandCursor: true })
-        .on('pointerover', () => menuBtn.setColor('#ff8888'))
-        .on('pointerout', () => menuBtn.setColor('#ffffff'))
-        .on('pointerdown', () => {
-            this.scene.stop("GameScene");   // cierro juego
-            this.scene.start("MenuScene");  // vuelvo al menú
+        congifBtn.on('pointerout', () => congifBtn.setScale(0.2));
+
+        congifBtn.on('pointerdown', () => {
+            this.sound.play('clickSound', { volume: GameSettings.sfxVolume });
+
+            // lanzar ajustes
+            this.scene.launch('ConfigurationScene', { previous: 'PauseScene' });
+
+            // traer configuracion arriba del todo
+            this.scene.bringToTop('ConfigurationScene');
+
+            // dormir menu de pausa
+            this.scene.sleep('PauseScene');
         });
+
+
+        // boton de salir al menu
+        const exitBtn = this.add.image(cx + 50, cy + 200, 'btnExit')
+            .setScale(0.7)
+            .setOrigin(0.5)
+            .setInteractive({ useHandCursor: true });
+
+        exitBtn.on('pointerover', () => { 
+            exitBtn.setScale(0.74);
+            this.sound.play('hoverSound', {
+                volume: GameSettings.sfxVolume,
+                detune: Phaser.Math.Between(-20, 20)
+            }); 
+        });
+
+        exitBtn.on('pointerout', () => exitBtn.setScale(0.7));
+
+        exitBtn.on('pointerdown', () => {
+            this.sound.play('clickSound', { volume: GameSettings.sfxVolume });
+
+            GameSettings.stopSfx();
+
+            // cerramos todo lo relacionado con el juego dividido
+            this.scene.stop('LeftScene');
+            this.scene.stop('RightScene');
+            this.scene.stop(this.parentSceneKey); // SplitScreenScene
+            this.scene.stop('PauseScene');
+
+            // volvemos al menu principal
+            this.scene.start('MenuScene');
+        });
+        
+        // boton de volver
+        const backBtn = this.add.image(cx + 700, cy + 300, 'btnBack')
+            .setScale(0.1)
+            .setOrigin(0.5)
+            .setInteractive({ useHandCursor: true });
+
+        backBtn.on('pointerover', () => { 
+            backBtn.setScale(0.14);
+        });
+
+        backBtn.on('pointerout', () => backBtn.setScale(0.1));
+
+        backBtn.on('pointerdown', () => {
+            this.sound.play('clickSound', { volume: GameSettings.sfxVolume });
+
+            // reanudar musica
+            GameSettings.resumeMusic();
+
+            // cerramos solo esta escena
+            this.scene.stop('PauseScene');
+
+            // le decimos a SplitScreenScene que haga togglePause
+            this.scene.get(this.parentSceneKey).togglePause();
+        });
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(this.escKey)) {
+            this.closePauseMenu();
+        }
+    }
+
+    closePauseMenu() {
+
+        // reanudar musica
+        GameSettings.resumeMusic();
+
+        // cerrar el menu de pausa
+        this.scene.stop(); // detiene PauseScene
+
+        // reanudar escenas del juego
+        const parent = 'SplitScreenScene';
+
+        this.scene.resume(parent);
+        this.scene.resume('LeftScene');
+        this.scene.resume('RightScene');
     }
 }
